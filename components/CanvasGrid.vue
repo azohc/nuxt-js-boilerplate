ceb7e016ddc0f589f9522a1a9c585d8c8580ca27
juancs <juanchozass@gmail.com>
2023-01-12 15:23:31 +0100

    Organize and clean up snake movement

--- components/CanvasGrid.vue
+++ components/CanvasGrid.vue
@@ -21,66 +21,65 @@ const props = defineProps({
     required: true,
   },
 })
-
-let playing
-let context
-const DIR_UP = "UP"
-const DIR_DOWN = "DOWN"
-const DIR_LEFT = "LEFT"
-const DIR_RIGHT = "RIGHT"
-
-const TICKRATE = 150
+const DIRECTION_UP = "UP"
+const DIRECTION_DOWN = "DOWN"
+const DIRECTION_LEFT = "LEFT"
+const DIRECTION_RIGHT = "RIGHT"
+const SNAKE_GREEN = "#4F822B"
+const BACKGROUND_GREEN = "#4F822B"
+const TICKRATE = 250
 
 // the below "PIXEL" is NxN actual atomic pixels
 const PIXEL_SIZE = 5
+
 // the below "SQUARE" is MxM of the above PIXELs
 const SQUARE_SIZE = 2
-const canvas = ref(null)
+
 const latestKeydown = ref(null)
+const canvas = ref(null)
+let context, playInterval
 
 const spawnPos = {
-  x: props.width / PIXEL_SIZE / SQUARE_SIZE,
-  y: props.height / PIXEL_SIZE / SQUARE_SIZE,
+  x: Math.floor(props.width / PIXEL_SIZE / SQUARE_SIZE),
+  y: Math.floor(props.height / PIXEL_SIZE / SQUARE_SIZE),
+}
+if (spawnPos.x % 2) {
+  spawnPos.x -= 1
+}
+if (spawnPos.y % 2) {
+  spawnPos.y -= 1
 }
 
 const snakeState = ref({
-  coords: [
+  coordinates: [
     spawnPos,
     { ...spawnPos, x: spawnPos.x - SQUARE_SIZE },
     { ...spawnPos, x: spawnPos.x - 2 * SQUARE_SIZE },
     { ...spawnPos, x: spawnPos.x - 3 * SQUARE_SIZE },
   ],
-  dir: DIR_RIGHT,
+  direction: DIRECTION_RIGHT,
 })
-
-const fillBg = (color) => {
-  context.fillStyle = color
-  context.fillRect(0, 0, props.width, props.height)
-}
 
 onMounted(() => {
   context = canvas.value.getContext("2d", {
     alpha: false,
   })
-  fillBg("#4F822B")
+  fillBg(BACKGROUND_GREEN)
 
-  const drawGreenPx = (x, y) => {
-    context.fillStyle = "#213404"
-    context.fillRect(
-      x * PIXEL_SIZE,
-      y * PIXEL_SIZE,
-      PIXEL_SIZE,
-      PIXEL_SIZE
-    )
-  }
-  drawGreenPx(5, 5)
+  playInterval = setInterval(onTick, TICKRATE)
 
-  playing = setInterval(onTick, TICKRATE)
   drawGrid({
     drawFirstLine: true,
-    color: "#4F822B88",
+    color: `${SNAKE_GREEN}88`,
   })
+
+  drawSnake()
 })
+
+function fillBg(color) {
+  context.fillStyle = color
+  context.fillRect(0, 0, props.width, props.height)
+}
 
 function drawGrid(options) {
   const { color, drawFirstLine } = options
@@ -103,94 +102,8 @@ function drawGrid(options) {
   context.strokeStyle = color
   context.stroke()
 }
-function drawSquare(x, y, color) {
-  drawPx(x, y, color)
-  drawPx(x, y + 1, color)
-  drawPx(x + 1, y, color)
-  drawPx(x + 1, y + 1, color)
-}
 
-function onTick() {
-  // consume whatever the latest keydown is
-  if (latestKeydown.value) {
-    console.log("latestkeydown:", latestKeydown.value)
-    latestKeydown.value = null
-  }
-  // DETERMINE NEW HEAD POSITION
-  unshiftNewHeadCoord()
-  // // POP TAIL // TODO UNLESS ATE APPLE ON THIS TICK
-  const pop = snakeState.value.coords.pop()
-  // // TODO DONT RESET, JUST ERASE SNAKE USING COORDS ARRAY
-  reset()
-  drawSnake()
-}
-
-function drawSnake() {
-  const X_CELLS = props.width / PIXEL_SIZE
-  const Y_CELLS = props.height / PIXEL_SIZE
-  const wrapxc = (xc) => {
-    if (xc >= X_CELLS) return xc - X_CELLS
-    if (xc < 0) return xc + X_CELLS
-    return xc
-  }
-  const wrapyc = (yc) => {
-    if (yc >= Y_CELLS) return yc - Y_CELLS
-    if (yc < 0) return yc + Y_CELLS
-    return yc
-  }
-
-  const { coords } = snakeState.value
-  const len = coords.length
-  const head = coords[0]
-
-  drawSquare(head.x, head.y)
-  coords
-    .slice(1, len - 1)
-    .forEach((coord) => drawSquare(wrapxc(coord.x), wrapyc(coord.y)))
-}
-
-function unshiftNewHeadCoord() {
-  const head = snakeState.value.coords[0]
-  switch (snakeState.value.dir) {
-    case DIR_UP:
-      snakeState.value.coords.unshift({
-        x: head.x,
-        y:
-          head.y > 0
-            ? head.y - SQUARE_SIZE
-            : props.height / PIXEL_SIZE - SQUARE_SIZE,
-      })
-      break
-    case DIR_DOWN:
-      snakeState.value.coords.unshift({
-        x: head.x,
-        y:
-          head.y + SQUARE_SIZE < props.height / PIXEL_SIZE
-            ? head.y + SQUARE_SIZE
-            : 0,
-      })
-      break
-    case DIR_LEFT:
-      snakeState.value.coords.unshift({
-        x:
-          head.x > 0
-            ? head.x - SQUARE_SIZE
-            : props.width / PIXEL_SIZE - SQUARE_SIZE,
-        y: head.y,
-      })
-      break
-    case DIR_RIGHT:
-      snakeState.value.coords.unshift({
-        x:
-          head.x + SQUARE_SIZE < props.width / PIXEL_SIZE
-            ? head.x + SQUARE_SIZE
-            : 0,
-        y: head.y,
-      })
-      break
-  }
-}
-function drawPx(x, y, color) {
+function drawPixel(x, y, color) {
   context.fillStyle = color ? color : "#213404"
   context.fillRect(
     x * PIXEL_SIZE,
@@ -199,25 +112,98 @@ function drawPx(x, y, color) {
     PIXEL_SIZE
   )
 }
-const reset = () => {
-  context.clearRect(0, 0, props.width, props.height)
-  fillBg("#4F822B")
+
+function drawSquare(x, y, color) {
+  drawPixel(x, y, color)
+  drawPixel(x, y + 1, color)
+  drawPixel(x + 1, y, color)
+  drawPixel(x + 1, y + 1, color)
 }
 
-const togglepause = () => {
-  if (playing == null) {
-    playing = setInterval(onTick, TICKRATE)
-    // TODO add indicator of paused state
-  } else {
-    playing = clearInterval(playing)
+function drawSnake() {
+  const { coordinates } = snakeState.value
+  const head = coordinates[0]
+
+  drawSquare(head.x, head.y)
+  coordinates
+    .slice(1)
+    .forEach((coord) => drawSquare(coord.x, coord.y))
+}
+
+function onTick() {
+  // consume whatever the latest keydown is
+  if (latestKeydown.value) {
+    const currentDirection = snakeState.value.direction
+    if (
+      (latestKeydown.value === DIRECTION_UP &&
+        currentDirection !== DIRECTION_DOWN) ||
+      (latestKeydown.value === DIRECTION_DOWN &&
+        currentDirection !== DIRECTION_UP) ||
+      (latestKeydown.value === DIRECTION_RIGHT &&
+        currentDirection !== DIRECTION_LEFT) ||
+      (latestKeydown.value === DIRECTION_LEFT &&
+        currentDirection !== DIRECTION_RIGHT)
+    ) {
+      snakeState.value.direction = latestKeydown.value
+    }
+    latestKeydown.value = null
+  }
+  setNextHeadCoordinates()
+  // // POP TAIL // TODO UNLESS ATE APPLE ON THIS TICK
+  const tail = snakeState.value.coordinates.pop()
+  // TODO avoid reset, just use popped tail to paint as bggreen
+  reset()
+  drawSnake()
+}
+
+function setNextHeadCoordinates() {
+  const currentHead = snakeState.value.coordinates[0]
+  switch (snakeState.value.direction) {
+    case DIRECTION_UP:
+      snakeState.value.coordinates.unshift({
+        x: currentHead.x,
+        y:
+          currentHead.y > 0
+            ? currentHead.y - SQUARE_SIZE
+            : props.height / PIXEL_SIZE - SQUARE_SIZE,
+      })
+      break
+    case DIRECTION_DOWN:
+      snakeState.value.coordinates.unshift({
+        x: currentHead.x,
+        y:
+          currentHead.y + SQUARE_SIZE < props.height / PIXEL_SIZE
+            ? currentHead.y + SQUARE_SIZE
+            : 0,
+      })
+      break
+    case DIRECTION_LEFT:
+      snakeState.value.coordinates.unshift({
+        x:
+          currentHead.x > 0
+            ? currentHead.x - SQUARE_SIZE
+            : props.width / PIXEL_SIZE - SQUARE_SIZE,
+        y: currentHead.y,
+      })
+      break
+    case DIRECTION_RIGHT:
+      snakeState.value.coordinates.unshift({
+        x:
+          currentHead.x + SQUARE_SIZE < props.width / PIXEL_SIZE
+            ? currentHead.x + SQUARE_SIZE
+            : 0,
+        y: currentHead.y,
+      })
+      break
   }
 }
-const onKeyDown = (event) => {
+
+function onKeyDown(event) {
   if (event.code === "Space") {
     togglepause()
     return
   }
-  const ACCEPTED_DIR_KEYCODES = [
+  const DIRECTION_KEYCODES = [
     "ArrowUp",
     "ArrowDown",
     "ArrowLeft",
@@ -231,21 +217,35 @@ const onKeyDown = (event) => {
     "KeyH",
     "KeyL",
   ]
-  const i = ACCEPTED_DIR_KEYCODES.indexOf(event.code)
+  const i = DIRECTION_KEYCODES.indexOf(event.code)
   switch (i % 4) {
     case 0:
-      latestKeydown.value = DIR_UP
+      latestKeydown.value = DIRECTION_UP
       break
     case 1:
-      latestKeydown.value = DIR_DOWN
+      latestKeydown.value = DIRECTION_DOWN
       break
     case 2:
-      latestKeydown.value = DIR_LEFT
+      latestKeydown.value = DIRECTION_LEFT
       break
     case 3:
-      latestKeydown.value = DIR_RIGHT
+      latestKeydown.value = DIRECTION_RIGHT
       break
     default:
+  }
+}
+
+const reset = () => {
+  context.clearRect(0, 0, props.width, props.height)
+  fillBg(SNAKE_GREEN)
+}
+
+const togglepause = () => {
+  if (playInterval == null) {
+    playInterval = setInterval(onTick, TICKRATE)
+    // TODO add indicator of paused state
+  } else {
+    playInterval = clearInterval(playInterval)
   }
 }
 </script>
